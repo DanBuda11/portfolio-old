f72dec36a0bbf3371477d3dddefb616e1a1d9aaa
Dan Buda <danbuda@gmail.com>
2017-06-19 19:23:01 -0500

    start styling header & footer

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
-	mobileToggle() {
-		let menu = document.querySelector('.nav-mobile');
-		if (menu.style.display === 'block') {
-			menu.style.display = 'none';
+	menuToggle(menu) {
+		let el = document.querySelector(menu);
+		if (el.style.display === 'block') {
+			el.style.display = 'none';
 		} else {
-			menu.style.display = 'block';
-		}
-	}
-
-	tabletToggle() {
-		let menu = document.querySelector('.nav-tablet-dropdown');
-		if (menu.style.display === 'block') {
-			menu.style.display = 'none';
-		} else {
-			menu.style.display = 'block';
+			el.style.display = 'block';
 		}
 	}
 
 	render() {
+		const github = 'https://github.com/danbuda11';
+		const codepen = 'https://codepen.io/danbuda/';
+		const linkedin = 'https://www.linkedin.com/in/danbuda/';
+		const twitter = 'https://twitter.com/danielbuda';
 		return (
 			<div className="nav">
-				<i className="fa fa-bars menu-mobile" onClick={this.mobileToggle}></i>
+				<i className="fa fa-bars fa-3x menu-mobile" onClick={() => this.menuToggle('.nav-mobile')}></i>
 				<div className="nav-mobile">
 					<div className="nav-mobile-1">
-						<a href="">Portfolio</a>
-						<a href="">About</a>
+						<Link to="/">Portfolio</Link>
+						<Link to="/about">About</Link>
 						<a href="">Resume</a>
 					</div>
 					<div className="nav-mobile-2">
@@ -34,47 +30,47 @@ export default class Header extends Component {
 						<a href="mailto:danbuda@gmail.com">Email</a>
 					</div>
 					<div className="nav-mobile-3">
-						<a href=""><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
-						<a href=""><i className="fa fa-codepen" aria-hidden="true"></i></a>
-						<a href=""><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-						<a href=""><i className="fa fa-github-square" aria-hidden="true"></i></a>
+						<a href={twitter} target="_blank"><i className="fa fa-2x fa-twitter-square" aria-hidden="true"></i></a>
+						<a href={codepen} target="_blank"><i className="fa fa-2x fa-codepen" aria-hidden="true"></i></a>
+						<a href={linkedin} target="_blank"><i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
+						<a href={github} target="_blank"><i className="fa fa-2x fa-github-square" aria-hidden="true"></i></a>
 					</div>
 				</div>
 				<div className="nav-tablet">
 					<div className="nav-tablet-1">
-						<a href="">Call</a>
-						<a href="">Email</a>
+						<a href="tel:+15126631673">Call</a>
+						<a href="mailto:danbuda@gmail.com">Email</a>
 					</div>
-					<i className="fa fa-bars menu-tablet" onClick={this.tabletToggle}></i>
+					<i className="fa fa-bars fa-3x menu-tablet" onClick={() => this.menuToggle('.nav-tablet-dropdown')}></i>
 					<div className="nav-tablet-dropdown">
 						<div className="nav-tablet-2">
-							<a href="">Portfolio</a>
-							<a href="">About</a>
+							<Link to="/">Portfolio</Link>
+							<Link to="/about">About</Link>
 							<a href="">Resume</a>
 						</div>
 						<div className="nav-tablet-3">
-							<a href=""><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
-							<a href=""><i className="fa fa-codepen" aria-hidden="true"></i></a>
-							<a href=""><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-							<a href=""><i className="fa fa-github-square" aria-hidden="true"></i></a>
+							<a href={twitter} target="_blank"><i className="fa fa-2x fa-twitter-square" aria-hidden="true"></i></a>
+							<a href={codepen} target="_blank"><i className="fa fa-2x fa-codepen" aria-hidden="true"></i></a>
+							<a href={linkedin} target="_blank"><i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
+							<a href={github} target="_blank"><i className="fa fa-2x fa-github-square" aria-hidden="true"></i></a>
 						</div>
 					</div>
 				</div>
 				<div className="nav-desktop">
 					<div className="nav-desktop-1">
-						<a href="">Portfolio</a>
-						<a href="">About</a>
+						<Link to="/">Portfolio</Link>
+						<Link to="/about">About</Link>
 						<a href="">Resume</a>
 					</div>
 					<div className="nav-desktop-2">
-						<a href="">Call</a>
-						<a href="">Email</a>
+						<a href="tel:+15126631673">Call</a>
+						<a href="mailto:danbuda@gmail.com">Email</a>
 					</div>
 					<div className="nav-desktop-3">
-						<a href=""><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
-						<a href=""><i className="fa fa-codepen" aria-hidden="true"></i></a>
-						<a href=""><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-						<a href=""><i className="fa fa-github-square" aria-hidden="true"></i></a>
+						<a href={twitter} target="_blank"><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
+						<a href={codepen} target="_blank"><i className="fa fa-codepen" aria-hidden="true"></i></a>
+						<a href={linkedin} target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
+						<a href={github} target="_blank"><i className="fa fa-github-square" aria-hidden="true"></i></a>
 					</div>
 				</div>
 			</div>
