4b92271aa36521cdfabb472d48e17255b0b12d7e
Dan Buda <danbuda@gmail.com>
2017-07-02 14:08:46 -0500

    add project images

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,7 +1,7 @@
 export default [
 	{
 		name: 'Home Finder',
-		image: 'test-image.png',
+		image: 'home-finder.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/real-estate-app',
 		liveLink: 'http://home-finder-lite.herokuapp.com/',
@@ -9,7 +9,7 @@ export default [
 	},
 	{
 		name: 'Buda Fooda',
-		image: 'test-image.png',
+		image: 'buda-fooda.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/recipes',
 		liveLink: 'http://recipes-danbuda.surge.sh/',
@@ -17,7 +17,7 @@ export default [
 	},
 	{
 		name: 'Responsive React Navbar',
-		image: 'test-image.png',
+		image: 'react-nav.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/collapsable-nav',
 		liveLink: 'http://reactnav-danbuda.surge.sh/',
@@ -25,7 +25,7 @@ export default [
 	},
 	{
 		name: 'Node Chat App',
-		image: 'test-image.png',
+		image: 'node-chat.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/node-chat-app',
 		liveLink: 'https://glacial-taiga-27544.herokuapp.com/',
@@ -33,7 +33,7 @@ export default [
 	},
 	{
 		name: 'React-Redux Webpack Framework',
-		image: 'test-image.png',
+		image: 'no-image.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/react-redux-webpack',
 		liveLink: '',
@@ -41,7 +41,7 @@ export default [
 	},
 	{
 		name: 'danbuda.com',
-		image: 'test-image.png',
+		image: 'no-image.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/portfolio3',
 		liveLink: 'http://danbuda.com/',
@@ -49,7 +49,7 @@ export default [
 	},
 	{
 		name: 'React Photo Album',
-		image: 'test-image.png',
+		image: 'react-photo.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/react-photo-album',
 		liveLink: 'http://reactphotoalbum-danbuda.surge.sh/',
@@ -57,18 +57,11 @@ export default [
 	},
 	{
 		name: 'Star Wars API App',
-		image: 'test-image.png',
+		image: 'swapi.png',
 		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
 		githubLink: 'https://github.com/DanBuda11/swapi',
 		liveLink: 'http://swapi-danbuda.surge.sh/',
 		tech: ['React']
-	},
-	{
-		name: 'Test Project',
-		image: 'test-image.png',
-		blurb: 'Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
-		githubLink: 'https://github.com/DanBuda11/swapi',
-		tech: ['React', 'Backbone', 'Sass', 'Webpack']
 	}
 ];
 
